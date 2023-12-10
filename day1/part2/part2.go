e74f1a29f745c22113d2130b48eaead601336f99
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2023-12-09 21:52:18 -0500

    working

--- day1/part2/part2.go
+++ day1/part2/part2.go
@@ -6,11 +6,20 @@ import (
 	"os"
 	"regexp"
 	"strconv"
-	"strings"
 	"time"
 )
 
-var parseKeys map[string]string = map[string]string{"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
+var parseKeys map[string]rune = map[string]rune{
+	"one":   '1',
+	"two":   '2',
+	"three": '3',
+	"four":  '4',
+	"five":  '5',
+	"six":   '6',
+	"seven": '7',
+	"eight": '8',
+	"nine":  '9',
+}
 
 // var pStr string = "[^onetwhrfuivsxg0-9]"
 
@@ -38,86 +47,48 @@ func ProcessFile(f *os.File) (int, error) {
 	sum := 0
 
 	for s.Scan() {
-		// rpStr, _ := regexp.Compile(pStr)
-		// pbs := rpStr.ReplaceAll([]byte(s.Text()), []byte(""))
+		str := parseController(s.Text())
 
-		ns, err := parseController(s.Text())
-		if err != nil {
-			return 0, err
-		}
-
-		rexp, err := regexp.Compile("[a-zA-Z]")
-		if err != nil {
-			return 0, err
-		}
-
-		bs := rexp.ReplaceAll([]byte(ns), []byte(""))
-		i, err := strconv.Atoi(fmt.Sprintf("%s%s", string(bs[0]), string(bs[len(bs)-1])))
-		if err != nil {
-			return 0, err
-		}
+		i, _ := strconv.Atoi(str)
 
 		sum += i
-		// fmt.Printf("org:%s\tns:%s\tbs:%s\ti:%d\tsum:%d\n", s.Text(), ns, bs, i, sum)
 	}
 	return sum, nil
 }
 
-func parseController(s string) (string, error) {
-	s = strings.ToLower(s)
+func parseController(s string) string {
 	r, _ := regexp.Compile("[0-9]")
 
-	if (r.MatchString(string([]byte(s)[0])) && r.MatchString(string([]byte(s)[len(s)-1]))) || len(s) < 3 {
-		return s, nil
-	}
+	var left rune
+	var right rune
 
-	if len(s) >= 3 && len(s) <= 5 {
-		for k := range parseKeys {
-			rx, _ := regexp.Compile(k)
-			if !rx.MatchString(s) {
-				return parseString(s), nil
+	for i, v := range s {
+		if r.MatchString(string(v)) {
+			if left == 0 {
+				left = v
+			}
+			right = v
+			continue
+		}
+
+		for pk, pv := range parseKeys {
+			if v == rune(pk[0]) {
+				if i+len(pk)-1 > len(s)-1 {
+					continue
+				}
+
+				if s[i:i+len(pk)] != pk {
+					continue
+				}
+
+				if left == 0 {
+					left = pv
+				}
+				right = pv
+				break
 			}
 		}
 	}
 
-	if r.MatchString(string([]byte(s)[0])) {
-		for k := range parseKeys {
-			// rx, _ := regexp.Compile(k)
-			if strings.HasSuffix(s, k) {
-				return parseString(string(s[0]) + s[len(s)-5:]), nil
-			}
-		}
-	} else if r.MatchString(string([]byte(s)[len(s)-1])) {
-		for k := range parseKeys {
-			// rx, _ := regexp.Compile(k)
-			if strings.HasPrefix(s, k) {
-				return parseString(s[:5] + string(s[len(s)-1])), nil
-			}
-
-			// if rx.Match([]byte(s[:5])) {
-			// 	fmt.Println(s)
-			// }
-		}
-	}
-
-	return parseString(s), nil
-}
-
-func parseString(s string) string {
-	for k, i := range parseKeys {
-		rx, _ := regexp.Compile(k)
-		// fmt.Printf("PARSER: %s\t%s\t%s\t\n", k, i, s)
-		if rx.MatchString(s) {
-			s = string(rx.ReplaceAll([]byte(s), []byte(i)))
-			// fmt.Printf("PARSER: %s\t%s\t%s\t\n", k, i, s)
-		}
-	}
-	// s = removeAlpha(s)
-	// fmt.Printf("PARSER: %s\t\n", s)
-	return s
-}
-
-func removeAlpha(s string) string {
-	rexp, _ := regexp.Compile("[a-zA-Z]")
-	return string(rexp.ReplaceAll([]byte(s), []byte("")))
+	return string(left) + string(right)
 }
